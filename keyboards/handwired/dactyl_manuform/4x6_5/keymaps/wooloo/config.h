35f26493cf3ac1d9e21fe6d4a05d02621d9c9336
42kal <130559322+42kal@users.noreply.github.com>
2023-10-11 11:55:56 +0200

    Update config.h

--- keyboards/handwired/dactyl_manuform/4x6_5/keymaps/wooloo/config.h
+++ keyboards/handwired/dactyl_manuform/4x6_5/keymaps/wooloo/config.h
@@ -33,9 +33,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //Rows are doubled up
 #define MATRIX_COLS 6
 //here lays the info that is important but can't put on here because i can't redefine it
-// # define MATRIX_ROWS 10
-// # define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
-// # define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
+# define MATRIX_ROWS 10
+# define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+# define MATRIX_ROW_PINS { F7, B1, B3, B2, B6 }
 
 #define MASTER_RIGHT
 
